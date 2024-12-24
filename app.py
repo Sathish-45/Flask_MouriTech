853bf447e1b97a023b20f59b99c04bc7b30bfc81
--help <sathishc0298@gmail.com>
2024-12-24 11:02:14 +0530

    Updated Flask Application, added authorization.

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app=Flask(__name__)
 CORS(app)
 
 BASE_URL=os.getenv('BASE_URL')
+API_KEY=os.getenv('API_KEY')
 
 # Swagger setup
 SWAGGER_URL = '/swagger'
@@ -21,30 +22,47 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
 )
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
+# Helper function for authentication
+def authenticate():
+    key = "Bearer "+str(request.headers.get("Authorization"))
+    if not key or key != f"Bearer {API_KEY}":
+        return jsonify({"error": "Unauthorized"}), 401
 
-@app.route('/coins', methods=['GET'])
+@app.route('/coins', methods=['POST'])
 def list_all_coins():
+    auth_reponse=authenticate()
+    if auth_reponse:
+        return auth_reponse
     per_page=request.form.get('per_page',10, type=int)
     page=request.form.get('page',1,type=int)
+    
     list_coins=requests.get(url=f"{BASE_URL}/coins/list", params={'per_page':per_page,'page':page})
     return jsonify(list_coins.json()),200
 
 @app.route('/categories', methods=['GET'])
 def list_coins_categories():
+    auth_reponse=authenticate()
+    if auth_reponse:
+        return auth_reponse
     list_categories=requests.get(url=f"{BASE_URL}/coins/categories")
     return jsonify(list_categories.json()),200
 
 @app.route('/coin/<id>', methods=['GET'])
 def coin(id):
+    auth_reponse=authenticate()
+    if auth_reponse:
+        return auth_reponse
     list_categories=requests.get(url=f"{BASE_URL}/coins/{id}")
     return jsonify(list_categories.json()),200
 
 @app.route('/coin/market', methods=['POST'])
 def market_data():
+    auth_reponse=authenticate()
+    if auth_reponse:
+        return auth_reponse
     vs_currency=request.form.get('vs_currency','cad')
     days=request.form.get('days',10,type=int)
     id=request.form.get('coin_id')
-    print(vs_currency,days)
     market_data=requests.get(url=f'{BASE_URL}/coins/{id}/market_chart',params={'vs_currency':vs_currency,'days':days})
     return jsonify(market_data.json()),200
 
